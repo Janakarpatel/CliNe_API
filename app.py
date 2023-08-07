feb9810947221f9ee27d675f302bebf0a3486da8
Janakar Patel <104451253+Janakarpatel@users.noreply.github.com>
2023-08-07 18:30:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ app = Flask(__name__)
 # CORS(app)
 
 
-@app.route('/')
-def index():
-    return "Welcome to our CliNe's authorized API! To access our services through the Chrome extension, simply follow on web store to integrate it seamlessly with your browser. We value intellectual property rights, and all data and content provided through our API are protected by copyright laws. By accessing and using our API, you agree to abide by the copyright terms and conditions. Thank you for choosing our service, Happy browsing! © [CliNe.AI] 2023. All rights reserved."
+# @app.route('/')
+# def index():
+#     return "Welcome to our CliNe's authorized API! To access our services through the Chrome extension, simply follow on web store to integrate it seamlessly with your browser. We value intellectual property rights, and all data and content provided through our API are protected by copyright laws. By accessing and using our API, you agree to abide by the copyright terms and conditions. Thank you for choosing our service, Happy browsing! © [CliNe.AI] 2023. All rights reserved."
 
 # Define the API route
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
     try:
         # Get the text data from the request body
